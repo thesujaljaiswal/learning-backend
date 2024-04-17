4a3227784151804d9c4da3d8b551e1696140a87d
anjaanid <sujj4646@gmail.com>
2024-04-17 22:07:38 +0530

    utils codes discussion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import dotenv from "dotenv";
-import mongoose from 'mongoose';
-import connectDB from './db/db.js';
+import mongoose from "mongoose";
+import connectDB from "./db/db.js";
+// import { app } from "./app.js";
 
 dotenv.config({
-    path: "./env"
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`app serving on ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db Connection Failed ", err);
+  });
